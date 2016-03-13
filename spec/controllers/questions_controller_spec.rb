8f2ca0032ad5f4d2595b54e29c240193d23e1799
Diana Gabriela Mendoza <dgmendoza17@gmail.com>
2016-03-13 14:32:43 -0500

    Assignment 33 Rails: More CRUD : completed

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -34,38 +34,93 @@ RSpec.describe QuestionsController, type: :controller do
     end
   end
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+#this just below is new, referenced from the solution video
+  describe "POST create" do
+    it "increases the number of Questions by 1" do
+      expect{ post :create, {question: {title: "Title", body: "Body", resolved: false}}}. to change(Question,:count).by(1)
+    end
+
+    it "assigns the new question to @question" do
+      post :create, {question: my_question.attributes}
+      expect(assigns(:question)).to eq Question.last
+    end
+
+    it "redirects to the new question" do
+      post :create, {question: my_question.attributes}
+      expect(response).to redirect_to Question.last
     end
   end
 
-  describe "GET #show" do
+# this is what I previously had here instead
+#   describe "GET #create" do
+#     it "returns http success" do
+#       get :create
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
+
+# this is my original method implementation. But it doesn't pass the test.
+  describe "GET show" do  #what is the difference between putting "show" and "#show"? #show is used when referring to models.
     it "returns http success" do
       get :show
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #edit" do
+  describe "GET edit" do
     it "returns http success" do
       get :edit
       expect(response).to have_http_status(:success)
     end
+
+    it "renders the #edit view" do
+      get :edit, {id: my_question.id}
+      expect(response).to render_template :edit
+    end
+
   end
 
-  describe "GET #update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
+#I think this one here is why I was having trouble seeding my questions with data.
+  describe "PUT update" do
+    it "updates the question with expected attributes" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+
+      put :update, id: my_question.id, question: { title: new_title, body: new_body, resolved: false }
+
+      updated_question = assigns(:question)
+      expect(updated_question.id).to eq my_question.id
+      expect(updated_question.title).to eq new_title
+      expect(updated_question.body).to eq new_body
+    end
+
+    it "redirects to the updated question" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+
+      put :update, id: my_question.id, question: { title: new_title, body: new_body, resolved: true }
+      expect(response).to redirect_to my_question
     end
   end
 
-  describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
+#this was my original method implementation for UPDATE. I used GET instead of PUT.
+  # describe "GET #update" do
+  #   it "returns http success" do
+  #     get :update
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+  describe "DELETE destroy" do
+    it "deletes the question" do
+      delete :destroy, {id: my_question.id}
+      count = Question.where({id: my_question.id}).size
+      expect(count).to eq 0
+    end
+
+    it "redirects to questions index" do
+      delete :destroy, {id: my_question.id}
+      expect(response).to redirect_to questions_path
     end
   end
 
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,18 +2,20 @@ require 'rails_helper'
 require 'random_data'
 
 RSpec.describe Question, type: :model do
-
-  let(:question) {Question.create!(title: "New Question Title", body: "New Question Body") }
   describe "attributes" do
+    let(:question) {Question.new(title: "New Question Title", body: "New Question Body", resolved: false) }
+
     it "responds to title" do
-      expect(question). to respond_to(:title)
+      expect(question).to respond_to(:title)
     end
 
     it "responds to body" do
       expect(question).to respond_to(:body)
     end
 
-    #test for "resolved" attribute goes here
+    it "should respond to resolved" do
+      expect(question).to respond_to(:resolved)
+    end
 
   end
 
