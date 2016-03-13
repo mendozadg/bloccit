108fa64b20b6834310ac6dcfe7a49433d1fc4fa7
Diana Gabriela Mendoza <dgmendoza17@gmail.com>
2016-03-13 15:42:51 -0500

    Merge conflict with Schema.rb file resolved; deleted old Schema.rb, rake db, and new Schema.rb was generated

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-# <<<<<<< HEAD ##I think I need both of these, because ..1157 is for the topics element (Checkpoint 34) and ..1140 is for the questions element.
 ActiveRecord::Schema.define(version: 20160312001157) do
-# =======
-ActiveRecord::Schema.define(version: 20160311031140) do
-# >>>>>>> assignment-33-RailsMoreCRUD
-
-  create_table "advertisements", force: :cascade do |t|
-    t.string   "title"
-    t.text     "copy"
-    t.integer  "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -56,16 +44,8 @@ ActiveRecord::Schema.define(version: 20160311031140) do
     t.string   "name"
     t.boolean  "public",      default: true
     t.text     "description"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-  end
-
-  create_table "questions", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.boolean  "resolved"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
