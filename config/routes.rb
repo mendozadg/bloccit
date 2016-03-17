8b3d36136eb315a3007ea814d2c96cf7e9ea7a71
Diana Gabriela Mendoza <dgmendoza17@gmail.com>
2016-03-16 23:03:19 -0500

    Check and re-do of Checkpoint 34 to ensure that previous conflicts with master merge did not change anything from this checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
 
-# <<<<<<< HEAD
   resources :topics do
     resources :posts, except: [:index]
   end
 
 
-# =======
   resources :questions
-
-  resources :posts
-# >>>>>>> assignment-33-RailsMoreCRUD
 
   get 'about' => 'welcome#about'
 
