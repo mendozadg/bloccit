d585f720758668aa78d21dd03f7db6fc6d670a5b
Diana Gabriela Mendoza <dgmendoza17@gmail.com>
2016-04-06 22:24:53 -0500

    Assignment 34: Rails Topics and Posts : minor edits from mentor meeting

--- app/controllers/sponsored_posts_controller.rb
+++ app/controllers/sponsored_posts_controller.rb
@@ -11,8 +11,8 @@ class SponsoredPostsController < ApplicationController
   def create
     @sponsored_post = SponsoredPost.new
     @sponsored_post.title = params[:sponsored_post][:title]
-    @sponsored_post.body = params[:post][:body]
-    @sponsored_topic = SponsoredTopic.find(params[:topic_id])
+    @sponsored_post.body = params[:sponsored_post][:body]
+    @topic = Topic.find(params[:topic_id])
     @sponsored_post.topic = @topic
 
     if @sponsored_post.save
@@ -26,6 +26,32 @@ class SponsoredPostsController < ApplicationController
 
   def edit
     @sponsored_post = SponsoredPost.find(params[:id])
+  end
+
+  def update
+    @sponsored_post = SponsoredPost.find(params[:id])
+    @sponsored_post.title = params[:sponsored_post][:title]
+    @sponsored_post.body = params[:sponsored_post][:body]
+
+    if @sponsored_post.save
+      flash[:notice] = "Sponsored Post was updated."
+      redirect_to [@sponsored_post.topic, @sponsored_post]
+    else
+      flash.now[:alert] = "There was an error saving the sponsored post. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @sponsored_post = SponsoredPost.find(params[:id])
+
+    if @sponsored_post.destroy
+      flash[:notice] = "\"#{@sponsored_post.title}\" was deleted succesfully."
+      redirect_to @sponsored_post.topic
+    else
+      flash.now[:alert] = "There was an error deleting the sponsored post."
+      render :show
+    end
   end
 
 end
