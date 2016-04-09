a93ea211d85da81d24e62a3476a60e069603c656
Diana Gabriela Mendoza <dgmendoza17@gmail.com>
2016-04-08 21:36:27 -0500

    Assignment 35 Rails: Validation : completed

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -7,6 +7,12 @@ RSpec.describe Topic, type: :model do
   it { is_expected.to have_many(:posts) }
   it { is_expected.to have_many(:sponsored_posts) } #Note to self: originally had error b/c I had it as "sponsoredpost" rather than sponsored_post
 
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:description) }
+
+  it { is_expected.to validate_length_of(:name).is_at_least(5) }
+  it { is_expected.to validate_length_of(:description).is_at_least(15) }
+
   describe "attributes" do
     it "responds to name" do
       expect(topic).to respond_to(:name)
