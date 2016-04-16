49cc474f3f5675d27ff1e7efb2c126ef75c20e61
Diana Gabriela Mendoza <dgmendoza17@gmail.com>
2016-04-16 00:47:40 -0500

    Checkpoint 39 Rails Posts and Users : in progress, need to fix sponsored posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
 require "random_data"
 
+5.times do
+	User.create!(
+	name: RandomData.random_name,
+	email: RandomData.random_email,
+	password: RandomData.random_sentence
+	)
+end
+users = User.all
 
 15.times do
 	Topic.create!(
@@ -11,6 +19,7 @@ topics =  Topic.all
 
 50.times do
 	Post.create!(
+		user: users.sample,
 		topic: topics.sample,
 		title: RandomData.random_sentence,
 		body: RandomData.random_paragraph
@@ -33,18 +42,25 @@ end
 end
 questions = Question.all
 
-15.times do
-	Sponsored_Post.create!(
-		topic: topics.sample,
-		title: RandomData.random_sentence,
-		body: RandomData.random_paragraph
-	)
-end
-sponsored_posts = Sponsored_Post.all
+# 15.times do
+# 	Sponsored_Post.create!(
+# 		topic: topics.sample,
+# 		title: RandomData.random_sentence,
+# 		body: RandomData.random_paragraph
+# 	)
+# end
+# sponsored_posts = Sponsored_Post.all
+
+user = User.first
+user.update_attributes!(
+	email: 'dgmendoza17@gmail.com',
+	password: 'helloworld'
+)
 
 puts "Seeds finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
 puts "#{Question.count} questions created"
-puts "#{Sponsored_Post.count} sponsored post created"
+# puts "#{Sponsored_Post.count} sponsored post created"
