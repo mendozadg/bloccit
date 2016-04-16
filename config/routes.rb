f85b63f202ae0dacb396b80b4500534e1da5ce3c
Diana Gabriela Mendoza <dgmendoza17@gmail.com>
2016-04-15 20:51:44 -0500

    Checkpoint 38 Rails Authentication Signing In : completed

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   resources :questions
   resources :users, only: [:new, :create]
 
+  resources :sessions, only: [:new, :create, :destroy]
+
   get 'about' => 'welcome#about'
 
   root to: "welcome#index"
