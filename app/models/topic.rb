cf5f5e30ca61c09bb4047089d1a8974c12318a7d
Diana Gabriela Mendoza <dgmendoza17@gmail.com>
2016-04-26 22:03:06 -0500

    Checkpoint 46 Rails: Private Topics : completed

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,6 +4,8 @@ class Topic < ActiveRecord::Base
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
 
-  validates :name, length: { minimum: 5 }, presence: true
-  validates :description, length: { minimum: 15 }, presence: true
+  scope :visible_to, -> (user) { user ? all : where(public: true) }
+
+  # validates :name, length: { minimum: 5 }, presence: true
+  # validates :description, length: { minimum: 15 }, presence: true
 end
